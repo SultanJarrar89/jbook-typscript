131ae768406623896fa8fa22f3027087dabf5be9
sultan <sultanjarrar89@gmail.com>
2021-05-03 00:25:22 -0700

    Using Refs for Arbitrary Values

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import * as esbuild from 'esbuild-wasm'
 
 const App = () => {
+  const ref = useRef<any>()
   const [input, setInput] = useState('')
   const [code, setCode] = useState('')
 
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wsam',
     })
-    console.log(service)
   }
   const onclick = () => {
-    console.log(input)
+    if (!ref.current) return
+    console.log(ref.current)
   }
 
   useEffect(() => {
