cf640d9017c40ceceba9182bfacd06303fcce459
sultan <sultanjarrar89@gmail.com>
2021-05-05 18:59:52 -0700

    Resolving Nested Paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import React, { useState, useEffect, useRef } from 'react'
-import * as esbuild from 'esbuild-wasm'
-import { unpkgPathPlugin } from './plugins/unpakg-path-plugin'
+import React, { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpakg-path-plugin';
 
 const App = () => {
-  const ref = useRef<any>()
-  const [input, setInput] = useState('')
-  const [code, setCode] = useState('')
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
       wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    })
-  }
+    });
+  };
   const onclick = async () => {
-    if (!ref.current) return
+    if (!ref.current) return;
 
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
       plugins: [unpkgPathPlugin()],
-    })
+    });
 
-    setCode(result.outputFiles[0].textgit )
-  }
+    setCode(result.outputFiles[0].text);
+  };
 
   useEffect(() => {
-    startService()
-  }, [])
+    startService();
+  }, []);
 
   return (
     <div>
@@ -41,7 +41,7 @@ const App = () => {
       </div>
       <pre>{code}</pre>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
