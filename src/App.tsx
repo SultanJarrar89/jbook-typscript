f0e7abcf22d4624b13bfd485cdd054e6d7538e68
sultan <sultanjarrar89@gmail.com>
2021-05-13 01:49:44 -0700

    Adding Resizable Panes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
-import React, { useState } from 'react';
-import CodeEditor from './components/CodeEditor';
-import Preview from './components/Preview';
-import bundle from './bundler';
-
+import CodeCell from './components/CodeCell';
 const App = () => {
-  const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
-
-  const onclick = async () => {
-    const output = await bundle(input);
-
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue='const a = 2'
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onclick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };
---
+++ src/components/CodeCell.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import CodeEditor from './CodeEditor';
+import Preview from './Preview';
+import bundle from '../bundler';
+
+const CodeCell = () => {
+  const [code, setCode] = useState('');
+  const [input, setInput] = useState('');
+
+  const onclick = async () => {
+    const output = await bundle(input);
+
+    setCode(output);
+  };
+  return (
+    <div>
+      <CodeEditor
+        initialValue='const a = 2'
+        onChange={(value) => setInput(value)}
+      />
+      <div>
+        <button onClick={onclick}>Submit</button>
+      </div>
+      <Preview code={code} />
+    </div>
+  );
+};
+
+export default CodeCell;
