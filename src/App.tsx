aa6164f0cfbeaf486d9291df380750f34c8218d9
sultan <sultanjarrar89@gmail.com>
2021-05-13 01:28:40 -0700

    Extracting Bundling Logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import CodeEditor from './components/CodeEditor';
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from './plugins/unpakg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 import Preview from './components/Preview';
+import bundle from './bundler';
 
 const App = () => {
-  const ref = useRef<any>();
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
   const onclick = async () => {
-    if (!ref.current) return;
+    const output = await bundle(input);
 
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-    setCode(result.outputFiles[0].text);
+    setCode(output);
   };
-
-  useEffect(() => {
-    startService();
-  }, []);
 
   return (
     <div>
@@ -42,10 +19,6 @@ const App = () => {
         initialValue='const a = 2'
         onChange={(value) => setInput(value)}
       />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
       <div>
         <button onClick={onclick}>Submit</button>
       </div>
