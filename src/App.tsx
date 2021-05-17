830094c0e6f05cf9ab8e28b7761e5dc9b226ff67
sultan <sultanjarrar89@gmail.com>
2021-05-17 00:11:33 -0700

    Adding Markdown Editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import CodeCell from './components/CodeCell';
+import TextEditor from './components/TextEditor';
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      {/* <CodeCell /> */}
+      <TextEditor />
     </div>
   );
 };
