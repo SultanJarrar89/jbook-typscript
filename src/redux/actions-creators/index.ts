32ad0d9373d27e7861fe7941bd92ae71fea8fc15
sultan <sultanjarrar89@gmail.com>
2021-05-18 21:48:15 -0700

    Defining Direction

--- src/redux/actions-creators/index.ts
+++ src/redux/actions-creators/index.ts
@@ -5,6 +5,7 @@ import {
   DeleteCellAction,
   MoveCellAction,
   InsertCellBeforeAction,
+  Direction,
 } from '../actions';
 import { CellTypes } from '../cell';
 
@@ -23,10 +24,7 @@ export const deleteCell = (id: string): DeleteCellAction => {
     payload: id,
   };
 };
-export const moveCell = (
-  id: string,
-  direction: 'up' | 'down'
-): MoveCellAction => {
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
   return {
     type: ActionType.MOVE_CELL,
     payload: { id, direction },
