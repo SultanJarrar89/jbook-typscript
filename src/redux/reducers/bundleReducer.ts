f2058164b4f8452f315c1d26c0b1ff8014d4ec9c
sultan <sultanjarrar89@gmail.com>
2021-05-24 11:37:29 -0700

    Initial bunldeReducer Implementation

--- src/redux/reducers/bundleReducer.ts
+++ src/redux/reducers/bundleReducer.ts
@@ -1,1 +1,27 @@
-export const t = 1;
+import { ActionType } from '../actions-types';
+import produce from 'immer';
+import { Action } from '../actions';
+
+export interface BundleState {
+  [key: string]: {
+    loading: boolean;
+    code: string;
+    err: string;
+  };
+}
+
+const initialState: BundleState = {};
+
+const bundleReducer = produce(
+  (state: BundleState = initialState, action: Action) => {
+    switch (action.type) {
+      case ActionType.BUNDLE_START:
+        return state;
+
+      case ActionType.BUNDLE_COMPLETE:
+        return state;
+      default:
+        return state;
+    }
+  }
+);
